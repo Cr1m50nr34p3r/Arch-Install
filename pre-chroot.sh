847d9b10efb4812dd46293699331a9010dbefc03
Cr1m50nr34p3r <akshatgarg789@gmail.com>
2022-01-29 09:26:47 +0530

    Updated Script

--- pre-chroot.sh
+++ pre-chroot.sh
@@ -17,8 +17,7 @@ case $UpMirror in
     pacman -Syy reflector rsync
     clear
     mirror_msg
-    reflector -c "India,*" --save /etc/pacman.d/mirrorlist
-    reflector --sort rate --threads 4 -l 200 | grep '^Server' >> /etc/pacman.d/mirrorlist
+    reflector --sort rate --threads $(($(nproc)+1)) -l 300 --verbose --save /etc/pacman.d/mirrorlist
     ;;
 'n' | 'N' | '')
     echo "Cantinuing to Partitioning ...."
@@ -31,18 +30,22 @@ part_msg () {
 	echo ""
 }
 part_msg
+lsblk
 printf "Whiich Drive do u wanna partition: "
 read disk
 cfdisk /dev/$disk
 clear &&  part_msg
 echo "FORMATTING AND MOUNTING PARTITIONS"
+lsblk
 printf "What is your EFI  partition: "
 read efi_part
 mkfs.fat -F32 /dev/$efi_part
+lsblk
 printf "What is your root partition: "
 read root_part
 mkfs.ext4 /dev/$root_part
 mount /dev/$root_part /mnt
+lsblk
 printf "do you have a Swap: "
 read is_swap
 case $is_swap in
@@ -60,10 +63,10 @@ clear
 echo "##############################"
 echo "### INSTALLING BASE SYSTEM ###"
 echo "##############################"
-pacstrap /mnt base linux linux-firmware
+pacstrap /mnt base linux linux-firmware linux-headers base-devel
 genfstab -U /mnt >> /mnt/etc/fstab
 clear
 echo "COPYING SCRIPT FILES ..... "
-cp -r $HOME/Arch-Install /mnt
+cp -rv $HOME/Arch-Install /mnt
 echo "CHANGING ROOT"
 arch-chroot /mnt
